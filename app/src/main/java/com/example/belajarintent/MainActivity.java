4f9da6a047c0c56f3df58b7d1edfea9798e914ad
Andhika Chandra R <andhikaacr7@gmail.com>
2025-03-23 04:35:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintent/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.belajarintent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button explicitintent;
+    Button implicitintent;
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+        explicitintent = (Button)findViewById(R.id.explicitintent);
+        explicitintent.setOnClickListener(this);
+        implicitintent = (Button)findViewById(R.id.implicitintent);
+        implicitintent.setOnClickListener(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.explicitintent) {
+            Intent explicit = new Intent(MainActivity.this, IntentActivity.class);
+            startActivity(explicit);
+        } else if (v.getId() == R.id.implicitintent) {
+            Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.w3schools.com"));
+            startActivity(implicit);
+        }
+    }
+}
